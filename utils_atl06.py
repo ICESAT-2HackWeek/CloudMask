ef32c65f730dcfbe665f51861ba09f4796cc4625
facusapienza21 <fsapienza@berkeley.edu>
2020-11-12 20:03:29 -0700

    I changed the name of the columns in the ATL06 dataframes in order to coincide with the name of the variables in the ATL06 data dictonary

--- utils_atl06.py
+++ utils_atl06.py
@@ -49,6 +49,7 @@ def read_atl06_fromfile(fname, add_flags=True, outdir='data', bbox=None):
                 # Ground Track
                 npts = len(data['segment_id']) # we use the number of segment_id as reference for the total number of measuraments
                 data['ground_track'] = [g[1:]] * npts
+                
                 # In order to know which one is the strong and weak beam, we need to know the orientation of tha spacecraft. ICESat-2 operates 
                 # between two different modes: forward and backward. For each one of this modes, the strong and weak will correspond to left and right 
                 # in a different way. For more details, see Section 7.5 in ATL03 Manual. 
@@ -62,42 +63,45 @@ def read_atl06_fromfile(fname, add_flags=True, outdir='data', bbox=None):
                 # Time
                 delta_t = fi[g+'/land_ice_segments/delta_time'][:]     # for time conversion
                 t_ref = fi['/ancillary_data/atlas_sdp_gps_epoch'][:]     # single value
-                t_gps = t_ref + delta_t  # Time in GPS seconds (secs since Jan 5, 1980)
-                data['time'] = gps2dyr(t_gps) # GPS sec to datetime
-
+                t_gps = t_ref + delta_t  # Time in GPS seconds (secs since Jan 6, 1980)
+                
+                # There are two different ways to obtain a time object from the timestamp. time_2 is 19sec ahead of time
+                data['time'] = [time_from_GPS(t) for t in t_gps]
+                data['time_2'] = gps2dyr(t_gps) # GPS sec to datetime
+                
                 ### General ###
 
                 # Latitude
-                data['lat'] = fi[g+'/land_ice_segments/latitude'][:]
+                data['latitude'] = fi[g+'/land_ice_segments/latitude'][:]
                 # Longitude
-                data['lon'] = fi[g+'/land_ice_segments/longitude'][:]
+                data['longitude'] = fi[g+'/land_ice_segments/longitude'][:]
                 # Standard land-ice segment height determined by land ice algorithm with bias corrections
-                data['height'] = fi[g+'/land_ice_segments/h_li'][:]            
+                data['h_li'] = fi[g+'/land_ice_segments/h_li'][:]            
                 # Along-track slope from along-track segment fit
-                data['slope_dx'] = fi[g+'/land_ice_segments/fit_statistics/dh_fit_dx'][:]
+                data['dh_fit_dx'] = fi[g+'/land_ice_segments/fit_statistics/dh_fit_dx'][:]
                 # Across-track slepe from weak and strong beam
-                data['slope_dy'] = fi[g+'/land_ice_segments/fit_statistics/dh_fit_dy'][:]
+                data['dh_fit_dy'] = fi[g+'/land_ice_segments/fit_statistics/dh_fit_dy'][:]
                 # Signal-to-noise ratio in the final refined window
                 data['snr'] = fi[g+'/land_ice_segments/fit_statistics/snr'][:]
 
                 ### Flags ###
 
                 # Quality flag. Zero indicates that no data-quality tests have found a problem 
-                data['q_flag'] = fi[g+'/land_ice_segments/atl06_quality_summary'][:]
+                data['atl06_quality_summary'] = fi[g+'/land_ice_segments/atl06_quality_summary'][:]
                 # The quality summary will be desactivated (=0) if all the following conditions are satisfied
                 # 1) h_robust_spread < 1 (meters)
                 # Robust dispersion estimator of misfit between photon events heights and the along track segment fit
-                data['h_rb'] = fi[g+'/land_ice_segments/fit_statistics/h_robust_sprd'][:]
+                data['h_robust_sprd'] = fi[g+'/land_ice_segments/fit_statistics/h_robust_sprd'][:]
                 # 2) h_li_sigma < 1 (meters)
                 # Propagation error due to sample error 
-                data['height_sigma'] = fi[g+'/land_ice_segments/h_li_sigma'][:]            
+                data['h_li_sigma'] = fi[g+'/land_ice_segments/h_li_sigma'][:]            
                 # 3) snr_significance < 0.02
                 # Probability that signal-finding routine would converge to at least the observed signal-to-noise (SNR) for a random
                 # noise input. 
                 data['snr_significance'] = fi[g+'/land_ice_segments/fit_statistics/snr_significance'][:]
                 # 4) Signal_selection_source \in {0,1}
                 # this variable takes values in {0,1,2,3} based on which algorithms was used to compute the heights, if possible
-                data['s_fg'] = fi[g+'/land_ice_segments/fit_statistics/signal_selection_source'][:]
+                data['signal_selection_source'] = fi[g+'/land_ice_segments/fit_statistics/signal_selection_source'][:]
                 # 5) n_fit_photons / W_surface_window_final > 1 Photon events / meter (weak beam)
                 #    n_fit_photons / W_surface_window_final > 4 Photon events / meter (strong beam)
                 data['n_fit_photons'] = fi[g+'/land_ice_segments/fit_statistics/n_fit_photons'][:]
@@ -107,22 +111,22 @@ def read_atl06_fromfile(fname, add_flags=True, outdir='data', bbox=None):
                 ### Blowing Snow ###
 
                 # Confidence flag from presence of blowing snow. 0 = Clear with high confidence, 1 = clear with medium confidence
-                data['blowing_snow_conf'] = fi[g+'/land_ice_segments/geophysical/bsnow_conf'][:]
+                data['bsnow_conf'] = fi[g+'/land_ice_segments/geophysical/bsnow_conf'][:]
                 # Blowing snow layer top height. This is zero in cases where such a layer is not detected (b_snow_conf = 1)
-                data['blowing_snow_h'] = fi[g+'/land_ice_segments/geophysical/bsnow_h'][:]
+                data['bsnow_h'] = fi[g+'/land_ice_segments/geophysical/bsnow_h'][:]
                 # Optical Tickness of blowing snow layer
-                data['blowing_snow_od'] = fi[g+'/land_ice_segments/geophysical/bsnow_od'][:]
+                data['bsnow_od'] = fi[g+'/land_ice_segments/geophysical/bsnow_od'][:]
 
                 ### Cloud= Flags ###
                 # Cloud flag (probably) from apparent surface reflectance based on ATL09
-                data['c_flg_asr'] = fi[g+'/land_ice_segments/geophysical/cloud_flg_asr'][:]
+                data['cloud_flg_asr'] = fi[g+'/land_ice_segments/geophysical/cloud_flg_asr'][:]
                 # Number of layers found from the backscatter profile using DDA layer finder
-                data['c_flg_atm'] = fi[g+'/land_ice_segments/geophysical/cloud_flg_atm'][:]
+                data['cloud_flg_atm'] = fi[g+'/land_ice_segments/geophysical/cloud_flg_atm'][:]
 
                 ### More Flags ###
 
                 # Multiple scattering warning flag
-                data['msw'] = fi[g+'/land_ice_segments/geophysical/msw_flag'][:]
+                data['msw_flag'] = fi[g+'/land_ice_segments/geophysical/msw_flag'][:]
                 # Consolidated cloud flag (combination of cloud_flg_atm, cloud_flg_asr, bsnow_conf) and it takes daytime/nightime into consideration
                 # 0 = likely abscence of clouds or blowing snow
                 data['layer_flag'] = fi[g+'/land_ice_segments/geophysical/layer_flag'][:]
@@ -130,9 +134,9 @@ def read_atl06_fromfile(fname, add_flags=True, outdir='data', bbox=None):
                 ### Backgrounds Photons ###
 
                 # background count rate
-                data['background'] = fi[g+'/land_ice_segments/geophysical/bckgrd'][:]
+                data['bckgrd'] = fi[g+'/land_ice_segments/geophysical/bckgrd'][:]
                 # Expected background count rate
-                data['background_expected'] = fi[g+'/land_ice_segments/geophysical/e_bckgrd'][:]
+                data['e_bckgrd'] = fi[g+'/land_ice_segments/geophysical/e_bckgrd'][:]
 
 
                 #data['end_geoseg'] = fi['/ancillary_data/end_geoseg'][:]
@@ -164,10 +168,10 @@ def read_atl06_fromfile(fname, add_flags=True, outdir='data', bbox=None):
 
                 if add_flags:
 
-                    df['q_flag_1'] = df.apply(lambda row: 1 if (row.h_rb >= 1) else 0, axis = 1)
-                    df['q_flag_2'] = df.apply(lambda row: 1 if (row.height_sigma >= 1) else 0, axis = 1)
+                    df['q_flag_1'] = df.apply(lambda row: 1 if (row.h_robust_sprd >= 1) else 0, axis = 1)
+                    df['q_flag_2'] = df.apply(lambda row: 1 if (row.h_li_sigma >= 1) else 0, axis = 1)
                     df['q_flag_3'] = df.apply(lambda row: 1 if (row.snr_significance >= 0.02) else 0, axis = 1)
-                    df['q_flag_4'] = df.apply(lambda row: 1 if (row.s_fg > 1) else 0, axis = 1)
+                    df['q_flag_4'] = df.apply(lambda row: 1 if (row.signal_selection_source > 1) else 0, axis = 1)
                     df['q_flag_5'] = df.apply(lambda row: 1 if ( (row.beam_strength == 'strong' and row.n_fit_photons_ratio_w <= 4) or (row.beam_strength == 'weak'   and row.n_fit_photons_ratio_w <= 1) ) else 0, axis = 1)
 
                 dataframes.append(df)
